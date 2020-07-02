2dbfb42c50e8578881be3a0f16f6ab68e6c44d67
matheushss1 <matheushenriquedesouzasilva@hotmail.com>
2020-07-02 15:37:47 -0300

    created user and users page

--- 02-database-manager/app.py
+++ 02-database-manager/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
@@ -12,14 +12,27 @@ class User(db.Model):
     name = db.Column(db.String(84), nullable= False)
     email = db.Column(db.String(150), nullable=False, unique=True, index=True)
     password = db.Column(db.String(256), nullable=False)
-
+    db
     def __str__(self):
         return self.name
 
 @app.route("/")
 def index():
     users = User.query.all() #select * from users;
-    return render_template("user.html", users=users)
+    return render_template("users.html", users=users)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    user = User.query.filter_by(id=id).first()
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/")
+
+@app.route("/user/<int:id>")
+def unique(id):
+    user = User.query.get(id)
+    return render_template("user.html", user=user)
 
 if __name__ == "__main__":
     app.run(debug=True)
